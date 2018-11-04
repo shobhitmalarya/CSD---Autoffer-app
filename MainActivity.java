5f6808b38829760d953d96c64919361cf4bd50d7
Shobhit Malarya <shobhit.m16@iiits.in>
2018-11-04 22:17:42 +0530

    initial .0

--- MainActivity.java
+++ MainActivity.java
@@ -8,13 +8,19 @@ import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.FirebaseException;
+import java.util.concurrent.TimeUnit;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthSettings;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    EditText phoneNumber = (EditText)findViewById(R.id.phnumber);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,4 +28,80 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    };
+    public void testPhoneVerify() {
+        // [START auth_test_phone_verify]
+        String phoneNum = "+917893445061";
+        String testVerificationCode = "123456";
+
+        // Whenever verification is triggered with the whitelisted number,
+        // provided it is not set for auto-retrieval, onCodeSent will be triggered.
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNum, 30L /*timeout*/, TimeUnit.SECONDS,
+                this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                    @Override
+                    public void onCodeSent(String verificationId,
+                                           PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        // Save the verification id somewhere
+                        // ...
+
+                        // The corresponding whitelisted code above should be used to complete sign-in.
+                        MainActivity.this.enableUserManuallyInputCode();
+                    }
+
+                    @Override
+                    public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                        // Sign in with the credential
+                        // ...
+                    }
+
+                    @Override
+                    public void onVerificationFailed(FirebaseException e) {
+                        // ...
+                    }
+
+                });
+        // [END auth_test_phone_verify]
+    }
+
+    private void enableUserManuallyInputCode() {
+        // No-op
+    }
+
+    public void testPhoneAutoRetrieve() {
+        // [START auth_test_phone_auto]
+        // The test phone number and code should be whitelisted in the console.
+        String phoneNumber = "+16505554567";
+        String smsCode = "123456";
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseAuthSettings firebaseAuthSettings = firebaseAuth.getFirebaseAuthSettings();
+
+        // Configure faking the auto-retrieval with the whitelisted numbers.
+        firebaseAuthSettings.setAutoRetrievedSmsCodeForPhoneNumber(phoneNumber, smsCode);
+
+        PhoneAuthProvider phoneAuthProvider = PhoneAuthProvider.getInstance();
+        phoneAuthProvider.verifyPhoneNumber(
+                phoneNumber,
+                60L,
+                TimeUnit.SECONDS,
+                this, /* activity */
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(PhoneAuthCredential credential) {
+                        // Instant verification is applied and a credential is directly returned.
+                        // ...
+                    }
+
+                    // [START_EXCLUDE]
+                    @Override
+                    public void onVerificationFailed(FirebaseException e) {
+
+                    }
+                    // [END_EXCLUDE]
+                });
+        // [END auth_test_phone_auto]
+    }
+
+
+};
