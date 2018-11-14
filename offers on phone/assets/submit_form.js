83c55cc0b199a9b55299daa7e0cc2425ed07fb29
Chinmay Samal <30817983+csDEVIL@users.noreply.github.com>
2018-11-14 23:42:27 +0530

    login is done from sells-man web portal
    check with multiple device at a atime is yet to be done

---
+++ offers on phone/assets/submit_form.js
@@ -0,0 +1,52 @@
+
+// Reference messages collection
+var messagesRef = firebase.database().ref(store_name);
+
+// Listen for form submit
+window.onload=function(){
+  document.getElementById('purchase_form').addEventListener('submit', submitForm);
+
+}
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var phone = getInputVal('phone');
+  var paid = getInputVal('paid');
+  var desc = getInputVal('desc');
+
+  //console.log(phone,paid,desc);
+
+  // Save message
+  saveMessage(phone, paid, desc);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block'; 
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },2000);
+
+  // Clear form
+  document.getElementById('purchase_form').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(phone, paid, desc){
+  var date=(Date(Date.now())).toString();
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    phone:phone,
+    paid:paid,
+    description:desc,
+    date:date
+  });
+}
